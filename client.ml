775f4d0abee7931b70ab2739f892baac3abc96af
dd1020 <david@david.localdomain>
2023-12-16 19:16:31 +0200

    Added roundtrip time and basic concurrency

--- client.ml
+++ client.ml
@@ -9,22 +9,31 @@ let () =
   let sock = socket PF_INET SOCK_STREAM 0 in
   try
     connect sock server_addr;
-    Printf.printf "Connected to the server. Type your messages:\n";
-    try
-      while true do
-        let msg = read_line () in
-        let sent_bytes = send sock (Bytes.of_string msg) 0 (String.length msg) [] in
-        if sent_bytes = 0 then raise End_of_file;
-        let buffer = Bytes.create buffer_size in
-        let recv_len = recv sock buffer 0 buffer_size [] in
-        if recv_len = 0 then raise End_of_file;
-        let response = Bytes.sub_string buffer 0 recv_len in
-        Printf.printf "Server response: %s\n" response;
-      done
-    with
-    | End_of_file | Unix_error (EPIPE, _, _) ->
-        Printf.printf "Connection to server lost.\n"
-    | exn -> Printf.printf "An error occurred while sending/receiving: %s\n" (Printexc.to_string exn)
+    let buffer = Bytes.create buffer_size in
+    let recv_len = recv sock buffer 0 buffer_size [] in
+    if recv_len = 0 then raise End_of_file;
+    let response = Bytes.sub_string buffer 0 recv_len in
+    Printf.printf "Server response: %s\n" response;
+    if response = "You are now connected to the server.\n" then
+      try
+        while true do
+          let msg = read_line () in
+          let t1 = Unix.gettimeofday () in
+          let sent_bytes = send sock (Bytes.of_string msg) 0 (String.length msg) [] in
+          if sent_bytes = 0 then raise End_of_file;
+          let recv_len = recv sock buffer 0 buffer_size [] in
+          if recv_len = 0 then raise End_of_file;
+          let t2 = Unix.gettimeofday () in
+          let response = Bytes.sub_string buffer 0 recv_len in
+          Printf.printf "Server response: %s\n" response;
+          Printf.printf "Round trip time: %f seconds\n" (t2 -. t1);
+        done
+      with
+      | End_of_file | Unix_error (EPIPE, _, _) ->
+          Printf.printf "Connection to server lost.\n"
+      | exn -> Printf.printf "An error occurred while sending/receiving: %s\n" (Printexc.to_string exn)
+    else
+      Printf.printf "Waiting for the server to be free...\n"
   with
   | Unix_error (err, _, _) when err = ECONNREFUSED ->
       Printf.printf "Could not connect to the server. Is it running?\n"
--- server.ml
+++ server.ml
@@ -1,37 +1,44 @@
 open Unix
 open Printf
+open Thread
 
 let buffer_size = 1024
 let queue = Queue.create ()
+let queue_mutex = Mutex.create ()
+let condition = Condition.create ()
+let active_client = ref None
 
-let inform_client_of_status sock is_active =
-  let msg = if is_active then "You are now connected to the server.\n" else "Waiting for your turn to connect...\n" in
+let inform_client_of_status sock msg =
   ignore (send sock (Bytes.of_string msg) 0 (String.length msg) [])
 
-let rec handle_client sock =
+let handle_client sock =
   let buffer = Bytes.create buffer_size in
   try
     while true do
       let recv_len = recv sock buffer 0 buffer_size [] in
       if recv_len = 0 then raise Exit;
       let msg = Bytes.sub_string buffer 0 recv_len in
-      printf "Received: %s\n" msg;
+      Printf.printf "Received: %s\n" msg;
       let response = "Message received" in
       ignore (send sock (Bytes.of_string response) 0 (String.length response) []);
     done
   with
-  | Exit -> printf "Client disconnected\n"
-  | exn -> printf "An error occurred: %s\n" (Printexc.to_string exn)
+  | Exit -> Printf.printf "Client disconnected\n%!"
+  | exn -> Printf.printf "An error occurred: %s\n%!" (Printexc.to_string exn);
+  close sock  (* Ensure socket is closed *)
 
 let rec serve_clients () =
-  match Queue.take_opt queue with
-  | Some client_sock ->
-      if Queue.length queue = 1 then
-        inform_client_of_status client_sock true;  (* Inform the client they are now being served *)
-      handle_client client_sock;
-      close client_sock;
-      serve_clients ()
-  | None -> ()
+  Mutex.lock queue_mutex;
+  while Queue.is_empty queue do
+    Condition.wait condition queue_mutex;
+  done;
+  let client_sock = Queue.take queue in
+  active_client := Some client_sock;
+  Mutex.unlock queue_mutex;
+  inform_client_of_status client_sock "You are now connected to the server.\n";
+  handle_client client_sock;
+  active_client := None;
+  serve_clients ()
 
 let () =
   let server_sock = socket PF_INET SOCK_STREAM 0 in
@@ -40,13 +47,17 @@ let () =
   let port = 8080 in
   bind server_sock (ADDR_INET (host_addr, port));
   listen server_sock 10;
-  printf "Server is listening on port %d\n" port;
+  Printf.printf "Server is listening on port %d\n" port;
+
+  let _ = create serve_clients () in
 
   while true do
     let (client_sock, _) = accept server_sock in
+    Mutex.lock queue_mutex;
     Queue.add client_sock queue;
-    if Queue.length queue = 1 then
-      serve_clients ()
+    if !active_client = None then
+      Condition.signal condition
     else
-      inform_client_of_status client_sock false;  (* Inform waiting clients *)
+      inform_client_of_status client_sock "Please wait, another client is being served.\n";
+    Mutex.unlock queue_mutex;
   done
